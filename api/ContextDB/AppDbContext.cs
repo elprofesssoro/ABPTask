5188f7f0f9f982b3c37d1571535efd15dfe5d009
I.D. <illia.netcore@gmail.com>
2026-09-21 12:58:39 +0200

    Added context db

---
+++ api/ContextDB/AppDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace api.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+
+    public DbSet<Hall> Halls => Set<Hall>();
+    public DbSet<Amenity> Amenities => Set<Amenity>();
+    public DbSet<Booking> Bookings => Set<Booking>();
+    public DbSet<HallAmenity> HallAmenities => Set<HallAmenity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<HallAmenity>()
+            .HasKey(ha => new { ha.HallId, ha.AmenityId });
+
+        modelBuilder.Entity<HallAmenity>()
+            .HasOne(ha => ha.Hall)
+            .WithMany(h => h.HallAmenities)
+            .HasForeignKey(ha => ha.HallId);
+
+        modelBuilder.Entity<HallAmenity>()
+            .HasOne(ha => ha.Amenity)
+            .WithMany(a => a.HallAmenities)
+            .HasForeignKey(ha => ha.AmenityId);
+    }
+}
